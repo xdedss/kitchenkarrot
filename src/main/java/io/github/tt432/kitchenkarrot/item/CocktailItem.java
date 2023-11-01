3d96ffab5be6b750efc5f5473780d76b4219ab8f
Zoshsgahdnkc <2987901713@qq.com>
2023-11-01 22:08:41 +0800

    fix tooltip, fix recipes, fix advancements

--- src/main/java/io/github/tt432/kitchenkarrot/item/CocktailItem.java
+++ src/main/java/io/github/tt432/kitchenkarrot/item/CocktailItem.java
@@ -1,5 +1,7 @@
 package io.github.tt432.kitchenkarrot.item;
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import io.github.tt432.kitchenkarrot.Kitchenkarrot;
 import io.github.tt432.kitchenkarrot.client.cocktail.CocktailList;
 import io.github.tt432.kitchenkarrot.config.ModCommonConfigs;
@@ -7,13 +9,19 @@ import io.github.tt432.kitchenkarrot.recipes.object.EffectStack;
 import io.github.tt432.kitchenkarrot.recipes.recipe.CocktailRecipe;
 import io.github.tt432.kitchenkarrot.registries.RecipeTypes;
 import io.github.tt432.kitchenkarrot.registries.ModItems;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffectUtil;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.alchemy.PotionUtils;
@@ -118,7 +126,58 @@ public class CocktailItem extends Item {
                 tooltip.add(Component.translatable("item.cocktail.author", recipe.author));
                 CocktailRecipe cocktailRecipe = get(level, name);
                 if (cocktailRecipe != null) {
-                    PotionUtils.addPotionTooltip(stack, tooltip, 1.0F);
+                    List<MobEffectInstance> list = cocktailRecipe.content.getEffect().stream().map(EffectStack::get).toList();
+                    if (list.isEmpty()) {
+                        tooltip.add(Component.translatable("effect.none").withStyle(ChatFormatting.GRAY));
+                    }
+                    else {
+                        List<Pair<Attribute, AttributeModifier>> list1 = Lists.newArrayList();
+                        for (MobEffectInstance mobeffectinstance : list) {
+                            MutableComponent mutablecomponent = Component.translatable(mobeffectinstance.getDescriptionId());
+                            MobEffect mobeffect = mobeffectinstance.getEffect();
+                            Map<Attribute, AttributeModifier> map = mobeffect.getAttributeModifiers();
+                            if (!map.isEmpty()) {
+                                for (Map.Entry<Attribute, AttributeModifier> entry : map.entrySet()) {
+                                    AttributeModifier attributemodifier = entry.getValue();
+                                    AttributeModifier attributemodifier1 = new AttributeModifier(attributemodifier.getName(), mobeffect.getAttributeModifierValue(mobeffectinstance.getAmplifier(), attributemodifier), attributemodifier.getOperation());
+                                    list1.add(new Pair<>(entry.getKey(), attributemodifier1));
+                                }
+                            }
+
+                            if (mobeffectinstance.getAmplifier() > 0) {
+                                mutablecomponent = Component.translatable("potion.withAmplifier", mutablecomponent, Component.translatable("potion.potency." + mobeffectinstance.getAmplifier()));
+                            }
+
+                            if (mobeffectinstance.getDuration() > 20) {
+                                mutablecomponent = Component.translatable("potion.withDuration", mutablecomponent, MobEffectUtil.formatDuration(mobeffectinstance, 1));
+                            }
+
+                            tooltip.add(mutablecomponent.withStyle(mobeffect.getCategory().getTooltipFormatting()));
+                        }
+
+                        if (!list1.isEmpty()) {
+                            tooltip.add(CommonComponents.EMPTY);
+                            tooltip.add(Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE));
+
+                            for (Pair<Attribute, AttributeModifier> pair : list1) {
+                                AttributeModifier attributemodifier2 = pair.getSecond();
+                                double d0 = attributemodifier2.getAmount();
+                                double d1;
+                                if (attributemodifier2.getOperation() != AttributeModifier.Operation.MULTIPLY_BASE && attributemodifier2.getOperation() != AttributeModifier.Operation.MULTIPLY_TOTAL) {
+                                    d1 = attributemodifier2.getAmount();
+                                } else {
+                                    d1 = attributemodifier2.getAmount() * 100.0D;
+                                }
+
+                                if (d0 > 0.0D) {
+                                    tooltip.add(Component.translatable("attribute.modifier.plus." + attributemodifier2.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(d1), Component.translatable(pair.getFirst().getDescriptionId())).withStyle(ChatFormatting.BLUE));
+                                } else if (d0 < 0.0D) {
+                                    d1 *= -1.0D;
+                                    tooltip.add(Component.translatable("attribute.modifier.take." + attributemodifier2.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(d1), Component.translatable(pair.getFirst().getDescriptionId())).withStyle(ChatFormatting.RED));
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
--- src/main/java/io/github/tt432/kitchenkarrot/item/ModFood.java
+++ src/main/java/io/github/tt432/kitchenkarrot/item/ModFood.java
@@ -1,10 +1,19 @@
 package io.github.tt432.kitchenkarrot.item;
 
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
 import io.github.tt432.kitchenkarrot.registries.ModItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffectUtil;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.alchemy.PotionUtils;
@@ -16,6 +25,7 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static io.github.tt432.kitchenkarrot.block.PlateHolderMap.plateHolder;
 
@@ -56,7 +66,54 @@ public class ModFood extends ModItem {
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
         super.appendHoverText(pStack, pLevel, tooltip, pIsAdvanced);
         if (this.effectEntries != null && this.effectEntries.length > 0) {
-            PotionUtils.addPotionTooltip(pStack, tooltip, 1.0F);
+            List<MobEffectInstance> list = Arrays.stream(this.effectEntries).map(effectEntry -> effectEntry.effect.get()).toList();
+            List<Pair<Attribute, AttributeModifier>> list1 = Lists.newArrayList();
+            for(MobEffectInstance mobeffectinstance : list) {
+                MutableComponent mutablecomponent = Component.translatable(mobeffectinstance.getDescriptionId());
+                MobEffect mobeffect = mobeffectinstance.getEffect();
+                Map<Attribute, AttributeModifier> map = mobeffect.getAttributeModifiers();
+                if (!map.isEmpty()) {
+                    for(Map.Entry<Attribute, AttributeModifier> entry : map.entrySet()) {
+                        AttributeModifier attributemodifier = entry.getValue();
+                        AttributeModifier attributemodifier1 = new AttributeModifier(attributemodifier.getName(), mobeffect.getAttributeModifierValue(mobeffectinstance.getAmplifier(), attributemodifier), attributemodifier.getOperation());
+                        list1.add(new Pair<>(entry.getKey(), attributemodifier1));
+                    }
+                }
+
+                if (mobeffectinstance.getAmplifier() > 0) {
+                    mutablecomponent = Component.translatable("potion.withAmplifier", mutablecomponent, Component.translatable("potion.potency." + mobeffectinstance.getAmplifier()));
+                }
+
+                if (mobeffectinstance.getDuration() > 20) {
+                    mutablecomponent = Component.translatable("potion.withDuration", mutablecomponent, MobEffectUtil.formatDuration(mobeffectinstance, 1));
+                }
+
+                tooltip.add(mutablecomponent.withStyle(mobeffect.getCategory().getTooltipFormatting()));
+            }
+
+            if (!list1.isEmpty()) {
+                tooltip.add(CommonComponents.EMPTY);
+                tooltip.add(Component.translatable("potion.whenDrank").withStyle(ChatFormatting.DARK_PURPLE));
+
+                for(Pair<Attribute, AttributeModifier> pair : list1) {
+                    AttributeModifier attributemodifier2 = pair.getSecond();
+                    double d0 = attributemodifier2.getAmount();
+                    double d1;
+                    if (attributemodifier2.getOperation() != AttributeModifier.Operation.MULTIPLY_BASE && attributemodifier2.getOperation() != AttributeModifier.Operation.MULTIPLY_TOTAL) {
+                        d1 = attributemodifier2.getAmount();
+                    } else {
+                        d1 = attributemodifier2.getAmount() * 100.0D;
+                    }
+
+                    if (d0 > 0.0D) {
+                        tooltip.add(Component.translatable("attribute.modifier.plus." + attributemodifier2.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(d1), Component.translatable(pair.getFirst().getDescriptionId())).withStyle(ChatFormatting.BLUE));
+                    } else if (d0 < 0.0D) {
+                        d1 *= -1.0D;
+                        tooltip.add(Component.translatable("attribute.modifier.take." + attributemodifier2.getOperation().toValue(), ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(d1), Component.translatable(pair.getFirst().getDescriptionId())).withStyle(ChatFormatting.RED));
+                    }
+                }
+            }
+
         }
     }
 
