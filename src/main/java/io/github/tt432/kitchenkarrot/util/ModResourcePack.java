6f2c562de5ac6fd4402d6cced8a3570d53945e6d
Zoshsgahdnkc <2987901713@qq.com>
2023-11-19 00:33:25 +0800

    adding resource pack & tags

---
+++ src/main/java/io/github/tt432/kitchenkarrot/util/ModResourcePack.java
@@ -0,0 +1,37 @@
+package io.github.tt432.kitchenkarrot.util;
+
+import io.github.tt432.kitchenkarrot.Kitchenkarrot;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraftforge.event.AddPackFindersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forgespi.locating.IModFile;
+import net.minecraftforge.resource.PathPackResources;
+
+import java.nio.file.Path;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModResourcePack {
+    @SubscribeEvent
+    public static void registerResourcePack(AddPackFindersEvent event) {
+        IModFile file = ModList.get().getModFileById(Kitchenkarrot.MOD_ID).getFile();
+        Path path = file.findResource("resourcepack/legacy_art");
+        event.addRepositorySource((consumer, packConstructor) -> {
+                Pack pack = Pack.create(
+                        new ResourceLocation(Kitchenkarrot.MOD_ID, "legacy_art").toString(),
+                        false,
+                        () -> new PathPackResources(Component.translatable("resource.kitchenkarrot.legacy_art").getString(), path),
+                        packConstructor,
+                        Pack.Position.TOP,
+                        PackSource.BUILT_IN
+                );
+                if (pack != null) consumer.accept(pack);
+            }
+        );
+    }
+}
